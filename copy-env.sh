1a69b9afc36efc1cecebc6196a9cfcbbffae2340
Przemek Kanach <kanach16@gmail.com>
2024-05-04 09:23:12 +0200

    Restructure

--- copy-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cp -r "$HOME/.config/alacritty" ./env/.config
-cp -r "$HOME/.config/i3" ./env/.config
-cp -r "$HOME/.config/i3status" ./env/.config
-cp -r "$HOME/.config/nvim" ./env/.config
-cp "$HOME/.zshrc" ./env
