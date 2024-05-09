237873f4597b0762e7423bc033727a6989771fa1
Przemek Kanach <kanach16@gmail.com>
2024-05-09 12:46:33 +0200

    Small change

--- neovim/.config/nvim/lua/przemo/remap.lua
+++ neovim/.config/nvim/lua/przemo/remap.lua
@@ -22,4 +22,5 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make the curr
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>",
     { desc = "Execute 'make_it_rain' animation" })
 
-vim.keymap.set("n", "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>", { desc = "Run tmux-sessionizer", silent = true })
+vim.keymap.set("n", "<C-f>", "<cmd>!tmux new-window tmux-sessionizer<CR>",
+    { desc = "Run tmux-sessionizer", silent = true })
