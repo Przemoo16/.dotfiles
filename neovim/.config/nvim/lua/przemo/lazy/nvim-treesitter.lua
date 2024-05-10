a004c28142e4fc8bafe51e754b4c17225a128670
Przemek Kanach <kanach16@gmail.com>
2024-05-10 14:14:12 +0200

    Add tmux cheat sheet

--- neovim/.config/nvim/lua/przemo/lazy/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/przemo/lazy/nvim-treesitter.lua
@@ -6,7 +6,8 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "bash", "c", "html", "javascript", "json", "lua", "python", "query", "rust", "typescript", "vimdoc"
+                "bash", "c", "html", "javascript", "json", "lua", "python", "query", "rust", "typescript", "vim",
+                "vimdoc"
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
