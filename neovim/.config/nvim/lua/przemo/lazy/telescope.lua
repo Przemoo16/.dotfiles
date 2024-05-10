e33c8cc20f5655466afb74259d116b00905092d6
Przemek Kanach <kanach16@gmail.com>
2024-05-10 15:30:25 +0200

    Show hidden files in telescope

--- neovim/.config/nvim/lua/przemo/lazy/telescope.lua
+++ neovim/.config/nvim/lua/przemo/lazy/telescope.lua
@@ -6,7 +6,26 @@ return {
     },
 
     config = function()
-        require('telescope').setup()
+        local telescopeConfig = require("telescope.config")
+        table.unpack = table.unpack or unpack
+        local vimgrep_arguments = { table.unpack(telescopeConfig.values.vimgrep_arguments) }
+
+        -- Search in hidden directories except of the .git one
+        table.insert(vimgrep_arguments, "--hidden")
+        table.insert(vimgrep_arguments, "--glob")
+        table.insert(vimgrep_arguments, "!**/.git/*")
+        require('telescope').setup({
+            defaults = {
+                -- `hidden = true` argument is not supported in text grep commands.
+                vimgrep_arguments = vimgrep_arguments,
+            },
+            pickers = {
+                find_files = {
+                    -- `hidden = true` argument will still show the inside of `.git/` as it's not `.gitignore`d.
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                },
+            },
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find files in a project" })
