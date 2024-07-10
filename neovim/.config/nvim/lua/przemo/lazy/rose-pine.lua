3f8194a1a781c07b52b3145e3f35a30ac1f41b3e
Przemek Kanach <kanach16@gmail.com>
2024-07-10 20:24:29 +0200

    Disable italic font

--- neovim/.config/nvim/lua/przemo/lazy/rose-pine.lua
+++ neovim/.config/nvim/lua/przemo/lazy/rose-pine.lua
@@ -3,8 +3,14 @@ return {
     name = "rose-pine",
 
     config = function()
+        require('rose-pine').setup({
+            styles = {
+                italic = false,
+            },
+        })
         vim.cmd("colorscheme rose-pine")
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
+
 }
